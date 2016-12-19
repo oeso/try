27572ae01633909a3a1584da401eb7fb21d0fc8a
oeso <osaraitne@gmail.com>
2016-12-19 17:00:35 +0900

    datepicker : input 박스에 선택 날짜 삽입시 1월이 0으로 표기되는 현상 수정

--- datepicker/js/calendar.js
+++ datepicker/js/calendar.js
@@ -2,7 +2,6 @@ var calendar = {
     slider : function(obj, date){
         function getElId(v){ return document.getElementById(v)};
 
-
         var date = ( typeof date == "undefined" )? new Date() : date;
             this.year = date.getFullYear();
             this.month = date.getMonth();
@@ -14,7 +13,6 @@ var calendar = {
         ,   endDate = new Array(31,28,31,30,31,30,31,31,30,31,30,31)
         ,   title = obj.lang == "en" ? ["sun","mon","tue","wed","tue","fri","sat"] : ["일","월","화","수","목","금","토"]
         ,   lastDate;
-
         //이 달의 마지막날짜. (마지막날을 모아둔 배열에서 현재 달 넘버에 해당하는 값)
 
         //윤년계산 ( 서력 기원 연수가 4로 나누어 떨어지는 해, 이 중에서 100으로 나누어 떨어지는 해는 평년, 400으로 나누어 떨어지는 해는 윤년 )
@@ -66,7 +64,7 @@ var calendar = {
                 }
                 this.classList.add("on")//element.classList.toggle() : toggle()은 classList 프로퍼티의 메소드
                 //선택날짜 하단 폼에 입력
-                formStart.value = that.year+"-"+that.month+"-"+this.innerHTML;
+                formStart.value = that.year+"-"+(that.month+1)+"-"+this.innerHTML;
             };
         };
 
