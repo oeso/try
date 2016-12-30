803c58dc839c1a19f41cb48e55364da7ea632e35
oeso <osaraitne@gmail.com>
2016-12-30 14:58:33 +0900

    datepicker : 1차 수정사항 진행 95%완료. 2016년을 마치며...2017 정유년에도 분석적으로 화팅!

--- datepicker/js/ottori-datepicker.js
+++ datepicker/js/ottori-datepicker.js
@@ -1,92 +1,77 @@
+function appendCreateEl(el, elName){
+    return el.appendChild(document.createElement(elName));
+};
+function addCreateTxtNode(el, elName){
+    return el.appendChild(document.createTextNode(elName));
+};
+function getElId(v){
+    return document.getElementById(v);
+};
 //본질이 무엇인지 파악하면서 짤 것.
 var calendar = {
-    getElId : function(v){
-        return document.getElementById(v);
-    },
     //calendar DOM 구조 만드는 함수
     action : function(obj, date){
-        this.calendarBox = calendar.getElId(obj.id);
-        //컨트롤 버튼과 년도월 표시 엘리먼트 생성하여 뿌림. 사용자 옵션을 다양하게 받아 구현할 수 있도로 따로 함수로 뺄 예정.
-        for(var i=0;i<4;i++){
-            var calendarControlBtn = this.calendarBox.appendChild(document.createElement("button"));
-            var calendarControlBtnFont = calendarControlBtn.appendChild(document.createElement("span"));
-            calendarControlBtn.className = "btn btn-primary";
+        this.calendarBox = getElId(obj.id);
+        this.lang = (obj.lang != "en") ? ["일","월","화","수","목","금","토"] : ["sun","mon","tue","wed","thu","fri","sat"];
 
-            if(i == 0){
-                calendarControlBtn.id = "prevYear";
-                calendarControlBtn.className = "btn btn-primary first";
-                calendarControlBtnFont.className = "glyphicon glyphicon-backward";
-            }else if( i==1 ){
-                calendarControlBtn.id = "prevMonth";
-                calendarControlBtn.className = "btn btn-info prev";
-                calendarControlBtnFont.className = "glyphicon glyphicon-chevron-left";
-            }else if( i==2 ){
-                calendarControlBtn.id = "nextMonth";
-                calendarControlBtn.className = "btn btn-info next";
-                calendarControlBtnFont.className = "glyphicon glyphicon-chevron-right";
-            }else if( i==3 ){
-                calendarControlBtn.id = "nextYear";
-                calendarControlBtn.className = "btn btn-primary last";
-                calendarControlBtnFont.className = "glyphicon glyphicon-forward";
-            };
+        //컨트롤 버튼과 년도월 표시 엘리먼트 생성하여 뿌림. 사용자 옵션을 다양하게 받아 구현할 수 있도록 수정 예정.
+        function buttonSetting(btnId, btnClass, btnChildClass){
+            var ControlBtn = appendCreateEl(calendar.calendarBox,"button");
+            var ControlBtnFont = appendCreateEl(ControlBtn,"span");
+
+            ControlBtn.id = btnId;
+            ControlBtn.className = btnClass;
+            ControlBtnFont.className = btnChildClass;
         };
-
+        buttonSetting("prevYear", "btn btn-primary first", "glyphicon glyphicon-backward");
+        buttonSetting("prevMonth", "btn btn-info prev", "glyphicon glyphicon-chevron-left");
+        buttonSetting("nextMonth", "btn btn-info next", "glyphicon glyphicon-chevron-right");
+        buttonSetting("nextYear", "btn btn-primary last", "glyphicon glyphicon-forward");
 
         //캘린더 연도와 월 표시할 엘리먼트 생성, 클래스 붙임. 사용자 옵션을 다양하게 받아 구현할 수 있도로 따로 함수로 뺄 예정.
-        var calendarTitle = this.calendarBox.appendChild(document.createElement("h2"));
+        var calendarTitle = appendCreateEl(this.calendarBox,"h2");
         calendarTitle.className = "text-center";
         calendarTitle.id = "yearTitle";
-
 
         var table = document.createElement("table");//table 엘리먼트 생성
 
         this.calendarBox.appendChild(table).setAttribute("id","table");//생성한 table을 div#calendar에 꽂아넣고 id="table" 부여함
-        this.table = calendar.getElId("table");//this.table에 꽂아넣은 table 엘리먼트를 담음.
+        this.table = getElId("table");//this.table에 꽂아넣은 table 엘리먼트를 담음.
         this.table.className = "table table-bordered";//this.table의 클래스 붙임.
         var head = this.table.createTHead();//table에 <head>생성하여 변수 head에 담음
         var row =  head.insertRow();//<head>에 <tr> 생성하여 변수 row에 담음
 
         //thead의 tr에 th 생성하고 class 부여.
         for(var i=0; i<7; i++){
-            var dayNameLink = row.appendChild(document.createElement("th"));
+            var dayNameLink = appendCreateEl(row,"th");
             dayNameLink.className = "text-center";
-            dayNameLink.appendChild(document.createTextNode("day"));//임시.
+            addCreateTxtNode(dayNameLink,this.lang[i]);//요일명은 임시
+
             //이 위치에 사용자 옵션선택에 따른 요일을 한글 또는 영문으로 뿌려주는 함수 호출 예정.
         };
 
-
         //table에 tbody 꽂아넣음
-        this.table.appendChild(document.createElement("tbody"));
+        appendCreateEl(this.table,"tbody");
         var tbodyEl= this.table.getElementsByTagName("tbody")[0];
         var tbodyRow = tbodyEl.insertRow();//tbody에 tr 생성
 
         //tbody의 tr에 td 생성
         for(var i=0; i<7; i++){
             var cell = tbodyRow.insertCell();
-            cell.appendChild(document.createElement("a"));
+            appendCreateEl(cell,"a");
         };
 
-        //tr에 id 값 부여함
-        tbodyRow.setAttribute("id","tableRow")//attribute 를 id가 아닌 class 또는 name 등 id가 아닌 속성을 지정했을 때는 바로 아래 클론이 되지 않는다. 왜 글치?
-        //var cloneRow = tableRow.cloneNode(true);//cloneNode의 parameter type은 boolean. default는 false. (true 사용시 속성은 물론 그 자손들까지 복제함.)
-
-        //해당 tr을 5번 복제하여 tbody에 꽂아넣음. id도 같이 복사됨 흑흑. id를 안넣어주면 cloneNode()가 안됨 흑흑. 왜 글치?
+        //해당 tr을 5번 복제하여 tbody에 꽂아넣음
         for(var i=0; i<5;i++){
-            tbodyEl.appendChild(tableRow.cloneNode(true));
+            tbodyEl.appendChild(tbodyRow.cloneNode(true));
         };
 
-
-        var todayBtn = this.calendarBox.appendChild(document.createElement("button"));
+        var todayBtn = appendCreateEl(this.calendarBox,"button");
         todayBtn.id = "today";
         todayBtn.className = "btn btn-danger btn-today";
         todayBtn.innerHTML = "TODAY";
 
         calendar.slider(obj, date);
-
-        console.log(           todayBtn.nodeName          ) //콘솔 찍어봐 ~
-
-
-
     },
     //만든 DOM 구조에 날짜를 뿌려주는 함수
     slider : function(obj, date){
@@ -105,9 +90,9 @@ var calendar = {
             start.setDate(1);//1일 세팅
 
         var startDayOfWeek = start.getDay()//1일의 요일
-        ,   formStart = calendar.getElId("startDay")
-        ,   formEnd = calendar.getElId("endDay")
-        ,   title = calendar.getElId("yearTitle");
+        ,   formStart = getElId("startDay")
+        ,   formEnd = getElId("endDay")
+        ,   title = getElId("yearTitle");
 
         //윤년계산 ( 서력 기원 연수가 4로 나누어 떨어지는 해는 윤년. 이 중 100으로 나 누어 떨어지는 해는 평년이며, 그 중 400으로 나누어 떨어지는 해는 윤년 )
         if( this.year % 4 === 0 ){
@@ -121,18 +106,22 @@ var calendar = {
         };
         lastDate = endDate[this.month];
 
-        //td 빈값 또는 a 엘리먼트 세팅
+        var days = 1;
+
+        //td에 a 엘리먼트 세팅 날짜 삽입
         for(var i=0; i<42; i++){
             if( i < startDayOfWeek || i >= lastDate + startDayOfWeek ){
                 calendarTd[i].innerHTML = "";
-            }else{
-                calendarTd[i].innerHTML = "<a href='#none'></a>";
-            }
-        };
-
-        //삽입한 a에 날짜 삽입
-        for(var i=1;i<=lastDate;i++){
-            calendarLink[i-1].innerHTML = i;
+            }else{ console.log( calendarTd[i].hasChildNodes() )
+                if( !calendarTd[i].hasChildNodes() ){
+                    addCreateTxtNode( appendCreateEl(calendarTd[i],"a"), days );
+                }else{
+                    var linkText = calendarTd[i].children[0].childNodes[0];
+                    if( linkText != null ){ linkText.remove() };
+                    addCreateTxtNode( calendarTd[i].children[0], days );
+                };
+                days++;
+            };
         };
 
         //today 표시
@@ -143,21 +132,21 @@ var calendar = {
             calendarLink[i].onclick = function(){
                 for(var k=0; k<length;k++){
                     calendarLink[k].classList.remove("on");
-                }
-                this.classList.add("on")
+                };
+                this.classList.add("on");
                 //선택날짜 하단 폼에 입력
-                formStart.value = thisCalendar.year+"-"+(thisCalendar.month+1)+"-"+this.innerHTML;
+                formStart.value = thisCalendar.year+"-"+(thisCalendar.month+1)+"-"+this.innerHTML;//이건 걍 innerHTML로 두겠음
             };
         };
 
         //현재 연도와 월 표시
-        title.innerHTML = this.year + " " + (this.month+1);
+        title.innerHTML = this.year + " " + (this.month+1);//이건 걍 innerHTML로 두겠음
 
-        var prevYearBtn = calendar.getElId("prevYear")
-        ,   prevMonthBtn = calendar.getElId("prevMonth")
-        ,   nextMonthBtn = calendar.getElId("nextMonth")
-        ,   nextYearBtn = calendar.getElId("nextYear")
-        ,   todayLink = calendar.getElId("today")
+        var prevYearBtn = getElId("prevYear")
+        ,   prevMonthBtn = getElId("prevMonth")
+        ,   nextMonthBtn = getElId("nextMonth")
+        ,   nextYearBtn = getElId("nextYear")
+        ,   todayLink = getElId("today")
         ,   thisCalendar = this
         ,   monthCount = this.month
         ,   prevMonth = thisCalendar.month -1
@@ -202,7 +191,3 @@ var calendar = {
         }
     }
 };
-calendar.action({
-    id : "calendar"
-});
-
