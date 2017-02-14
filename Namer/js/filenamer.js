3f21a6920bfcf35a64368302000ff69d20699d24
oeso <osaraitne@gmail.com>
2017-02-14 18:19:58 +0900

    FileNamer : 객체지향이 아닌 방식으로 변경.

--- Namer/js/filenamer.js
+++ Namer/js/filenamer.js
@@ -3,7 +3,8 @@
     Date : 2017 02 07
 */
 
-var fileListTable = document.getElementById("fileListTable");//table id
+
+
 var inputFile = document.getElementById("inputFile");//input file
 
 //좌측 메뉴
@@ -27,14 +28,101 @@ function nameSort(file){
     return filename;
 };
 
-/* checkbox cilck시 해당 row를 수집 */
-function checkedList(checkbox){
-    checkbox.onchange = function(){
-        if( this.value ){
-            fileNamerJS.selectList.push(this.parentElement.parentElement );
+function checking(checkbox, value){
+    if( value ){
+        checkbox.name = "marked";
+    }else{
+        checkbox.name = "";
+    };
+};
+//전체 선택
+function checkAll(checkbox){
+    var inputCheckbox = tbody.getElementsByTagName("input");
+
+    for( var i = 0 ; i < inputCheckbox.length; i++ ){
+        if( checkbox.checked ){
+            inputCheckbox[i].checked = true;
         }else{
-            fileNamerJS.selectList.push(this.parentElement.parentElement );
-        }
+            inputCheckbox[i].checked = false;
+        };
+    };
+};
+var fileListTable; //table id
+var tbody; //table tbody
+var fileNameList = [];
+var selectList = [];
+
+function fileOpenStart(input){
+    fileListTable = document.getElementById("fileListTable");//table id
+    tbody = fileListTable.tBodies[0];
+
+    console.log( input.files )
+    if( input.files ){//IE9 이상 , 그 외 브라우저
+        for(var i=0; i<input.files.length;i++){
+            //불러오면 일단 원래 파일명 리스트를 저장해둔다. 원복 가능해야 하므로.
+            fileNameList.push(input.files[i].name);
+        };
+    }else{//IE9 이하( File List 객체가 없음 )
+        fileNameList.push( nameSort( input.value ) );
+    };
+    document.getElementById("counting").innerHTML = "File open : " + fileNameList;
+    writeInTable( input );//dom요소를 만들어 뿌리는 함수 호출
+};
+
+var cnt = 0;
+function writeInTable( input ){
+
+    if( input.files ){ //IE9 이상 , 그 외
+        var repeat = input.files.length
+    }else{ //IE9 이하
+        var repeat = 1;
+    };
+
+    for( var i = 0; i < repeat; i++ ){ //input file로 불러온 file 갯수만큼 루프
+
+        var row = tbody.insertRow(); // tr 하나 생성
+
+        for(var k=0; k<4; k++){//tr 안에서 5번 루프
+            var cell = row.insertCell();//td 5개 생성
+
+            if( k == 0 ){//첫번째 td 에 checkbox 생성하여 type과 id 속성 추가
+                var check = cell.appendChild(document.createElement("input"));
+                check.setAttribute( "type" , "checkbox" );
+                check.setAttribute( "id" , "checkbox" + cnt );
+                check.parentElement.parentElement.id = "tr" + cnt;
+                check.addEventListener( "click" , function(){
+                    checking( this, this.checked );
+                });
+
+            }else if( k ==1){//세번째 td에
+                var cellLabel = cell.appendChild(document.createElement("label"));//label 생성 후
+                cellLabel.setAttribute( "for" , "checkbox" + cnt);
+                cellLabel.className = "oldName" ;
+
+                //label text로 file명 삽입
+                if( input.files ){ //IE9 이상 , 그 외
+                    cellLabel.innerHTML = input.files[i].name;
+                }else{ //IE9 이하
+                    cellLabel.innerHTML = input.value;
+                };
+            };
+        };
+        cnt++;
+    };
+};
+/* 목록 전체 삭제 */
+function deleteAll(){
+    tbody.innerHTML = ""; //tbody 하위에있는 tr 전체를 빈문자열로 덮어씀으로써 제거.
+};
+/* 목록 선택 삭제 */
+function deleteList(){
+    console.log( tbody )
+    //체크된 tr 수집
+    var checkedBox = document.getElementsByName("marked");
+
+    for( var i = 0 ; i < checkedBox.length; i++ ){
+        var checkedTr = checkedBox[i].parentElement.parentElement;
+        tbody.removeChild( checkedTr );
     };
 };
 
@@ -56,6 +144,7 @@ var fileNamerJS = {
             }else{//IE9 이하( File List 객체가 없음 )
                 fileNamerJS.fileNameList.push( nameSort( this.value ) );
             };
+            document.getElementById("counting").innerHTML = "File open : "+fileNamerJS.fileNameList
 
             fileNamerJS.writeInTable(fileListTable);//dom요소를 만들어 뿌리는 함수 호출
         };
@@ -66,7 +155,7 @@ var fileNamerJS = {
     },
     /* table에 불러온 file list를 dom요소를 만들어 뿌리기 */
     writeInTable : function(el){
-        this.tbody = el.tBodies.item[0];
+        this.tbody = el.tBodies[0];
 
         if( inputFile.files ){ //IE9 이상 , 그 외
             var repeat = inputFile.files.length
@@ -75,23 +164,23 @@ var fileNamerJS = {
         };
 
         for( var i = 0; i < repeat; i++ ){ //input file로 불러온 file 갯수만큼 루프
+
             var row = this.tbody.insertRow(); // tr 하나 생성
 
-            for(var k=0; k<5; k++){//tr 안에서 5번 루프
+            for(var k=0; k<4; k++){//tr 안에서 5번 루프
                 var cell = row.insertCell();//td 5개 생성
 
                 if( k == 0 ){//첫번째 td 에 checkbox 생성하여 type과 id 속성 추가
                     var check = cell.appendChild(document.createElement("input"));
                     check.setAttribute("type","checkbox");
-                    check.name = "check"
                     check.setAttribute("id","checkbox"+i);
+                    check.parentElement.parentElement.id = "tr"+i;
+                    check.addEventListener("click", function(){
+                        console.log(11)
+                        checking( this, this.checked );
+                    });
 
-                    checkedList(check);
-
-                }else if( k ==1){//두번째 td에
-                    cell.innerHTML = i;//넘버 생성
-
-                }else if( k ==2){//세번째 td에
+                }else if( k ==1){//세번째 td에
                     var cellLabel = cell.appendChild(document.createElement("label"));//label 생성 후
                     cellLabel.setAttribute("for","checkbox"+i);
                     cellLabel.className = "oldName";
@@ -108,11 +197,27 @@ var fileNamerJS = {
     },
     /* 목록 전체 삭제 */
     deleteAll : function(){
+        //목록 전체 삭제이므로 tbody 하위에있는 tr 전체를 빈문자열로 덮어씀으로써 제거.
         this.tbody.innerHTML = "";
     },
     /* 목록 선택 삭제 */
     deleteList : function(){
-        tr.deleteCell(); // deleteCell() : talbeRow Object Methods. Delete a cell from the current table row
+        //체크된 tr 수집
+        //function checkedItemSort(){
+            var checkedBox = fileNamerJS.tbody.getElementsByName("marked");
+
+            for( var i = 0 ; i < checkedBox.length; i++ ){
+                var checkedTr = checkedBox.parentElement.parentElement;
+                this.tbody.removeChild( checkedTr );
+            };
+        //};
+
+        // for( var i = 0 ; i < checkedItemSort().length; i++ ){
+        //     console.log( checkedItemSort()[i] );
+        //     console.log( fileNamerJS.tbody.rows[ checkedItemSort()[i] ] )
+        //     fileNamerJS.tbody.rows[ checkedItemSort()[i] ].innerHTML = "";
+        // };
+
     },
     /* 원래이름으로 */
     stringChange : function(filelist){
@@ -129,7 +234,7 @@ var fileNamerJS = {
     /* 앞이름 붙이기 */
     pasteBefore : function(filelist){
 
-    },
+    },// 2017 02 14
     /* 숫자만 남기기 */
     keepNumber : function(filelist){
 
