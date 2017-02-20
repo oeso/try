21f552dddd695fd44cb871a5379fb40ac21f94e4
oeso <osaraitne@gmail.com>
2017-02-20 18:07:41 +0900

    Firebase database 연동예정. 현재 localhost 에서 테스트 중

--- Namer/js/filenamer.js
+++ Namer/js/filenamer.js
@@ -1,20 +1,20 @@
 /**
  * Title : File Name Editor JS
  * Date : 2017 02 13~21
-**/
-var fileListTable; // 편집 테이블의 id
-var tbody; // 편집 테이블의 tbody
-var fileNameList = []; //open한 file 네임 리스트
-var layer; // 레이어
-var beforeValue; // 변경전 입력폼
-var afterValue; // 변경할 입력폼
-var navBtns; // 왼쪽메뉴버튼
-var labels; // 새 레이블
-var btnOk; // 레이어 확인 버튼
-var beforeEvent; // 왼쪽메뉴버튼 클릭시 일어나는 이벤트를 담아둘 변수
-var cnt = 0; // checkbox 와 tr, label id를 뿌릴때 사용. cnt는 계속 올라감
-var openMessage;
-var coreAction; // selectAction 함수 안에서 처리되어지는 부분으로 핵심 함수
+ **/
+var fileListTable // 편집 테이블의 id
+   , tbody // 편집 테이블의 tbody
+   , fileNameList = [] //open한 file 네임 리스트
+   , layer // 레이어
+   , beforeValue // 변경전 입력폼
+   , afterValue // 변경할 입력폼
+   , navBtns // 왼쪽메뉴버튼
+   , labels // 새 레이블
+   , btnOk // 레이어 확인 버튼
+   , beforeEvent // 왼쪽메뉴버튼 클릭시 일어나는 이벤트를 담아둘 변수
+   , cnt = 0 // checkbox 와 tr,  label id를 뿌릴때 사용. cnt는 계속 올라감
+   , openMessage
+   , coreAction // selectAction 함수 안에서 처리되어지는 부분으로 핵심 함수
 
 //두 개 이상의함수에서 쓰는 변수들은 onload시 미리 초기화.
 window.onload = function(){
@@ -26,9 +26,9 @@ window.onload = function(){
     btnOk         =  document.getElementById("btnOk"); // 레이어 확인 버튼
     openMessage   =  document.getElementById("openMessage"); // 레이어 확인 버튼
     navBtns       =  document.getElementById("nav").getElementsByTagName("button"); // 왼쪽메뉴버튼
-    //문자열 바꾸기 레이어 입력폼 내
-    closeLayer();
-    navButtonDisable();
+
+    closeLayer(); //새로고침시 문자열 바꾸기 레이어 입력폼 내용 초기화
+    navButtonDisable(); //새로고침시 왼쪽 메뉴 버튼 초기화(비활성화)
 };
 
 /* 현재 리스트에 파일이 없을 경우 왼쪽 메뉴 비활성화 */
@@ -46,11 +46,11 @@ function navButtonEnable(){
     };
     openMessage.style.display = "none";
 };
+var storageRef, imgRef,fileName ,mountainImagesRef, spaceRef, file,uploadTask, path, nm;
 
 /* file open시  */
 function fileOpenStart(input){
     navButtonEnable(); // left menu 버튼 활성화 한다.
-
     if( input.files ){ // IE9 이상 , 그 외 브라우저
         for(var i=0; i<input.files.length;i++){ // 불러오면 일단 원래 파일명 리스트를 저장해둔다. 원복 가능해야 하므로.
             fileNameList.push(input.files[i].name);
@@ -58,7 +58,7 @@ function fileOpenStart(input){
     }else{ // IE9 이하( File List 객체가 없음 )
         fileNameList = nameSort( input.value );
     };
-    writeInTable( input ); // Table에 삽입하는 함수 호출
+	writeInTable( input ); // Table에 삽입하는 함수 호출
 };
 
 /* 오픈한 file을 테이블명을 골라내어 Table 요소에 차례대로 삽입 */
@@ -68,15 +68,14 @@ function writeInTable( input ){
     }else{ //IE9 이하
         var repeat = 1;
     };
-
     for( var i = 0; i < repeat; i++ ){ //input file로 불러온 file 갯수만큼 루프
 
         var row = tbody.insertRow(); // tr 하나 생성
 
-        for(var k=0; k<4; k++){//tr 안에서 5번 루프
-            var cell = row.insertCell();//td 5개 생성
+        for(var k=0; k<4; k++){ // tr 안에서 5번 루프
+            var cell = row.insertCell(); // td 5개 생성
 
-            if( k == 0 ){//첫번째 td 에 checkbox 생성하여 type과 id 속성 추가
+            if( k == 0 ){ //첫번째 td 에 checkbox 생성하여 type과 id 속성 추가
                 var check = cell.appendChild(document.createElement("input"));
                 check.setAttribute( "type" , "checkbox" );
                 check.setAttribute( "id" , "checkbox" + cnt );
@@ -98,14 +97,12 @@ function writeInTable( input ){
             }else if( k == 2 ) {//세번째 td에
                 var labels = cell.appendChild(document.createElement("div"));//label 생성 후
                 labels.className = "newName" ;
-
-                //label text로 file명 삽입
-                if( input.files ){ //IE9 이상 , 그 외
+                if( input.files ){ //IE9 이상 , 그 외 label text로 file명 삽입
                     labels.innerHTML = input.files[i].name;
                 }else{ //IE9 이하
                     labels.innerHTML = fileNameList;
                 };
-            }
+            };
         };
         cnt++;
     };
@@ -153,10 +150,8 @@ function deleteAll(){
 
 /* 선택삭제 함수 : 체크된 tr 수집하여 체크된 항목이 없을 때까지 가장 위에 있는 tr 삭제 */
 function deleteRows(){
-
     if( selection().length == tbody.rows.length ){ // 선택항목과 전체항목 갯수가 같을경우 == 전체를 선택했을 시
         deleteAll(); // 일괄삭제 함수 호출
-
     }else{
         do{
             var checkedTr = selection()[0].parentElement.parentElement; // 체크된 첫번째 tr.
@@ -180,14 +175,12 @@ function restore(){
 
 /* 문자열 바꾸기 : layer */
 function stringChange( btn ){
-    removeEvent();
-    openLayer( btn );
-
-    btnOk.addEventListener("click", function(){
+    removeEvent(); // 그 전에 layer ok 버튼에 걸려있던 event 제거
+    openLayer( btn ); // layer 열기
+    btnOk.addEventListener("click", function(){ // 열린 layer ok 버튼에 이벤트 추가
         stringChangeAction();
     });
-
-    beforeEvent = stringChangeAction;
+    beforeEvent = stringChangeAction; // beforeEvent에 현재 이벤트 담아둠
 };
 
 /* 앞이름 붙이기 : prompt */
@@ -209,7 +202,7 @@ function pasteAfter(){
         return false;
     };
     coreAction = function( labels, nextxt ){ // 뒷이름 붙이기 함수 정의
-        labels.innerHTML = returnFileName(labels.innerHTML) + nextxt + ext(labels.innerHTML);
+        labels.innerHTML = returnFileName(labels.innerHTML) + nextxt + "." +ext(labels.innerHTML);
     };
     selectAction(nextxt);
 };
@@ -219,13 +212,13 @@ function keepNumber(){
     coreAction = function( labels ){
         var mix = returnFileName(labels.innerHTML).split(""); // 파일명 가져와서 배열로 만듦
         var nums = "";
-        for( var i = 0 ; i< mix.length; i ++){
-            var numbers = Number( mix[i] );
-            if( !isNaN( numbers ) && typeof numbers == "number" ){
-                nums += numbers;
+        for( var i = 0 ; i< mix.length; i ++){ // 파일명 문자 하나하나 돌면서
+            var numbers = Number( mix[i] ); // 파일명 문자 하나하나를 숫자로 변환
+            if( !isNaN( numbers ) && typeof numbers == "number" ){ // 각 문자타입이 number 이면서 NaN이 아니면
+                nums += numbers; // nums에 해당 문자열을 추가
             };
         };
-        labels.innerHTML = nums + ext(labels.innerHTML);
+        labels.innerHTML = nums + "." + ext(labels.innerHTML); // labels innerHTML 에 (합한 문자열과 본래 확장자 합하여) 삽입
     };
     selectAction();
 };
@@ -271,10 +264,10 @@ function delExtension(){
     selectAction();
 };
 
-/* 확장자만 리턴 */
-function ext(txt){
+/* 확장자만 리턴. 확장자 없을 시 원래 내용 리턴 */
+function ext(txt){ // 인자로 NewName의 innerHTML을 받음
     var start = txt.lastIndexOf("."); // 가장 마지막에 위치안 "."의 위치 값 number로 반환. lastIndexOf(), substr() 둘다 원래 문자열을 변경하지 않음
-    var extension = txt.substr( start , txt.length).toLowerCase(); // error 방지를 위하여 소문자로 ^^
+    var extension = txt.substr( start+1 , txt.length).toLowerCase(); // error 방지를 위하여 소문자로 ^^
     if( start != -1 ) { // txt (txt는 label.innerHTML) 내용 중에 "." 으로 구분되는 내용이 있을 경우  (없으면 -1 반환)
         return extension; // 순수 확장자만 리턴
     }else{
@@ -283,7 +276,7 @@ function ext(txt){
 };
 
 /* 확장자 제외한 파일명만 리턴 */
-function returnFileName(txt){
+function returnFileName(txt){ // 인자로 NewName의 innerHTML을 받음
     var lastDot = txt.lastIndexOf("."); // 가장 마지막에 위치안 "."의 위치 값 number로 반환. lastIndexOf(), substr() 둘다 원래 문자열을 변경하지 않음
     var purename = txt.substr( 0 , lastDot).toLowerCase();
     if( lastDot != -1 ) { // txt (txt는 label.innerHTML) 내용 중에 "." 으로 구분되는 내용이 있을 경우  (없으면 -1 반환)
@@ -295,14 +288,14 @@ function returnFileName(txt){
 
 var sortt;
 function listSorting() {
-    var arr1 = [], arr2 = [];
+    var arr1 = [], arr2 = []; //임의의 배열을 만들고
     for (var i = 0; i < tbody.rows.length; i++) {
         var oldLabel = tbody.rows[i].cells[1].getElementsByTagName("label")[0].innerHTML; // 초기 oldName값
         var newName = tbody.rows[i].cells[2].getElementsByTagName("div")[0].innerHTML; // 초기 newName값
-        arr1.push(oldLabel);
-        arr2.push(newName);
+        arr1.push(oldLabel); // 각 배열에 초기 oldName값 push
+        arr2.push(newName); // 각 배열에 // 초기 newName값 push
     };
-    sortt(arr1, arr2);
+    sortt(arr1, arr2); // sortt 함수 호출
     for (var i = 0; i < tbody.rows.length; i++) {
         tbody.rows[i].cells[1].getElementsByTagName("label")[0].innerHTML = arr1[i];
         tbody.rows[i].cells[2].getElementsByTagName("div")[0].innerHTML = arr2[i];
@@ -312,8 +305,8 @@ function listSorting() {
 /* 오름차순 정렬 : no layer */
 function sortAsc(){
     sortt = function(arr1, arr2){
-        arr1.sort();
-        arr2.sort();
+        arr1.sort(); // 초기 oldName값 오름차순 정렬
+        arr2.sort(); // 초기 newName값 오름차순 정렬
     };
     listSorting();
 };
@@ -322,15 +315,12 @@ function sortAsc(){
 function sortDesc(){
     sortt = function(arr1, arr2) {
         arr1.sort();
+        arr1.reverse(); // 초기 oldName값 오름차순 정렬 후 뒤집음
         arr2.sort();
-        arr1.reverse();
-        arr2.reverse();
+        arr2.reverse(); // 초기 newName값 오름차순 정렬 후 뒤집음
     };
     listSorting();
 };
-
-
-/** layer popup 후의 관련 Action **/
 
 /* 각 OldName의 내용을 편집하고자 하는 내용으로 교체하는 함수 */
 function selectAction( before,after ){
@@ -340,6 +330,15 @@ function selectAction( before,after ){
 
         coreAction( labels , before , after ); // 이거이 핵심,알맹이
     };
+};
+
+/* SAVE 누를시 실행 함수 */
+function save(){
+//Fire Base database 연동 예정
+//   firebase.database().ref('filename/' + userId).set({
+//     filename : name,
+//     extension : email
+//   });
 };
 
 /* 문자열 바꾸는 함수 */
@@ -365,59 +364,59 @@ function stringChangeAction(){
     };
 };
 
-/* 레이어 열기 함수 */
+/* layer 뜰 시 현재 이벤트 실행 전에 발생한 이벤트 삭제 */
+function removeEvent(){ // 바로 전에 등록했던 이벤트가 없으면 return false
+    if( !beforeEvent ){return false;}
+    btnOk.removeEventListener("click", beforeEvent ); // OK 버튼에 등록했던 전 이벤트 삭제.
+};
+
+/* layer 열기 */
 function openLayer( btn ){
-    document.getElementById("dim").style.display = "block";
-    document.getElementById("layerTitle").innerHTML = btn.innerHTML;
-    layer.style.display = "block";
-    beforeValue.focus();
+    document.getElementById("dim").style.display = "block"; // layer 뒤에 dim 요소 show
+    document.getElementById("layerTitle").innerHTML = btn.innerHTML; // layer title은 왼쪽 버튼의 내용으로 함.
+    layer.style.display = "block"; // 숨겨뒀던 layer show.
+    beforeValue.focus(); // 처음 입력폼에 포커스 ON
 };
 
-/* 레이어 닫기 1 */
+/* layer 닫기 1 */
 function closeLayer(){
-    beforeValue.value = "";
-    afterValue.value = "";
-    layer.style.display = "none";
-    document.getElementById("dim").style.display = "none";
+    beforeValue.value = ""; // before 입력폼 값 제거
+    afterValue.value = ""; // after 입력폼 값 제거
+    layer.style.display = "none"; // layer hide.
+    document.getElementById("dim").style.display = "none"; // dim 요소 hide
 };
 
-/* 레이어 닫기 1 */
+/* layer 닫기 2 */
 function closeLayerAction(){
-    if( beforeValue.value || afterValue.value ){
-        if( confirm("Do yo really want to close it?") ){
-            closeLayer();
+    if( beforeValue.value || afterValue.value ){ // before, after 어느 한쪽이라도 입력한 내용이 있으면
+        if( confirm("Do yo really want to close it?") ){ // 정말 닫을 것인지 확인 창 띄움.
+            closeLayer();// layer 닫음
         };
-    }else{
-        closeLayer();
+    }else{ // before, after 둘 다 입력 내용 없으면
+        closeLayer(); // layer 바로 닫음
     };
 };
 
-/* 레이어 닫기 2 : ESC or ENTER 닫기 */
+/* 레이어 닫기 3 : ESC or ENTER 닫기 */
 document.onkeydown = function(event) {
     if (layer.style.display != "none") { // layer 활성화 상태일 경우로 국한
         var x = event.which || event.keyCode; //Fire Fox 에서 keyCode 속성은 onkeypress 이벤트에서 미작동. which는 크로스브라우징을 위한 것.
 
-        if (x == 27) {
+        if (x == 27) { // ESC 키 누를시
             closeLayerAction();
-        }else if ( x == 13 ){
-            if( !beforeValue.value ){
+        }else if ( x == 13 ){ // ENTER 키 누를 시
+            if( !beforeValue.value ){ // before 항목 미입력시 alert
                 alert("Please enter before label");
                 return false;
-            }else if( !afterValue.value ){
+            }else if( !afterValue.value ){ // after 항목 미입력시 alert
                 alert("Please enter after label");
                 return false;
             }
-            beforeEvent();
+            beforeEvent(); // 왼쪽 메뉴 누를시 해당 기능에 해당하는 이벤트 실행 ( 왼쪽 메뉴 누를시 beforeEvent에 담아뒀던 이벤트)
         };
     };
 };
 
-/* layer 뜰 시 현재 이벤트 실행 전에 발생한 이벤트 삭제 */
-function removeEvent(){
-    if( !beforeEvent ){return false;}
-    btnOk.removeEventListener("click", beforeEvent );
-};
 
-function save(){
-    console.log("SAVE!!!!!!!! Fire BASE?????? What is it? hahaha ")
-}
+
+
